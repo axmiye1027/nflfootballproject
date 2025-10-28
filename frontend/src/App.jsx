33b03d275103964a4b130c8445dcbbade6b91e94
Ansuperope <aspenfbn@yahoo.com>
2025-10-28 08:25:43 -0700

    started frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,48 @@
+// homepage - first page to appear when opened
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      {/* NAVIGATION */}
+      <div className="row">
+        <a className="nav-btn" href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img className="nav-img" alt="Vite logo" src={viteLogo} />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <a className="nav-btn" href="https://react.dev" target="_blank" rel="noreferrer">
+          <img className="nav-btn" alt="React logo" src={reactLogo} />
+        </a>
+
+        <a className="nav-btn" href="https://react.dev" target="_blank" rel="noreferrer">
+          <img className="nav-btn" alt="React logo" src={reactLogo} />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
+      <img className="nav-btn" alt="search button"
+          src="https://preview.redd.it/why-does-scootaloos-cutie-mark-have-a-wing-v0-ruz6lnq3z64c1.png?width=885&format=png&auto=webp&s=e151e0d62c13c97e5fab681a9a2077f38d93117f" />
+        
+        <img className="nav-btn" alt="admin button"
+          src="https://derpicdn.net/img/2016/8/9/1221208/full.png" />
+
+        <img className="nav-btn" alt="search button"
+          src="https://derpicdn.net/img/2016/4/23/1138655/large.png" />
+
+      {/* HEADER */}
+      <div className="title">List of Teams</div>
+
+      {/* SEARCH - ICON - FILTERS */}
+      <div className="row">
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/* TEAM LIST */}
     </>
   )
 }
